0a27729fffe4909398ab8f7f58157b7a320f063c
Kai Lyons <kai@potabi.com>
2022-03-09 14:13:11 -0700

    Create virbos.h

---
+++ virbos/virbos.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define start {
+#define end   }
+#define display printf
+#define fn int
+#define stop ;
+#define nl display("\n")
+#define open  (
+#define close )
+#define opcl  ()
