a715ce18634325f66345e5abffcd9ed914e8b9d6
Kai Lyons <kai@potabi.com>
2022-03-09 14:40:35 -0700

    Create hacklon.h

---
+++ hacklon/hacklon.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#define stdns using namespace std
+#define display cout <<
+#define start {
+#define stop }
+#define r return
+#define open (
+#define close )
+#define stl <<
+#define end ;
+#define function int
+#define zero 0
+#define el << endl
+#define s char*
+#define elif else if
+#define set =
+#define equals ==
